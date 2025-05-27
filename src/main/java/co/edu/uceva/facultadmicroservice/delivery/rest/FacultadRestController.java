0fb0a70a0c9ba0115487ce6a79c259f88bc1929f
Nicolle Daniela Valverde Gallego <nicolle.valverde01@uceva.edu.co>
2025-05-27 14:59:54 -0500

    Update RestController

--- src/main/java/co/edu/uceva/facultadmicroservice/delivery/rest/FacultadRestController.java
+++ src/main/java/co/edu/uceva/facultadmicroservice/delivery/rest/FacultadRestController.java
@@ -5,10 +5,7 @@ import co.edu.uceva.facultadmicroservice.domain.entities.UsuarioDTO;
 import co.edu.uceva.facultadmicroservice.domain.exception.*;
 import co.edu.uceva.facultadmicroservice.domain.service.IFacultadService;
 import co.edu.uceva.facultadmicroservice.domain.service.IUsuarioClient;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,11 +14,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1/Facultad-Microservice")
 public class FacultadRestController {
@@ -33,7 +30,6 @@ public class FacultadRestController {
     private static final String MENSAJE = "mensaje";
     private static final String FACULTAD = "facultad";
     private static final String FACULTADES = "facultades";
-    private static final String USUARIOS = "usuarios";
 
 
     // Inyección de dependencia del servicio que proporciona servicios de CRUD
@@ -56,26 +52,6 @@ public class FacultadRestController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/decanos")
-    public ResponseEntity<Map<String, Object>> getDocentes() {
-        ObjectMapper mapper = new ObjectMapper();
-        //https://stackoverflow.com/questions/28821715/java-lang-classcastexception-java-util-linkedhashmap-cannot-be-cast-to-com-test
-        List<UsuarioDTO> usuarios = mapper.convertValue(usuarioClient.getUsuarios().getBody().get(USUARIOS), new TypeReference<List<UsuarioDTO>>(){});
-        List<UsuarioDTO> decanos = new ArrayList<>();
-        Map<String, Object> response = new HashMap<>();
-
-        for(UsuarioDTO usuario : usuarios) {
-            if(usuario.getRol().equals("Decano")) {
-                decanos.add(usuario);
-            }
-        }
-        if (decanos.isEmpty()) {
-            throw new NoHayDecanosException();
-        }
-        response.put(USUARIOS, decanos);
-        return ResponseEntity.ok(response);
-    }
-
     /**
      * Listar facultades con paginación.
      */
@@ -88,6 +64,7 @@ public class FacultadRestController {
         }
         return ResponseEntity.ok(facultades);
     }
+
     /**
      * Crear una nueva facultad pasando el objeto en el cuerpo de la petición.
      */
@@ -96,8 +73,9 @@ public class FacultadRestController {
         if (result.hasErrors()) {
             throw new ValidationException(result);
         }
-        Map<String, Object> response = new HashMap<>();
+        comprobarDecanos(facultad.getId_decano());
         Facultad nuevaFacultad = facultadService.save(facultad);
+        Map<String, Object> response = new HashMap<>();
         response.put(MENSAJE, "La facultad ha sido creado con éxito!");
         response.put(FACULTAD, nuevaFacultad);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -120,6 +98,7 @@ public class FacultadRestController {
 
     /**
      * Actualizar una facultad pasando el objeto en el cuerpo de la petición.
+     *
      * @param facultad: Objeto Facultad que se va a actualizar
      */
     @PutMapping("/facultades")
@@ -127,6 +106,7 @@ public class FacultadRestController {
         if (result.hasErrors()) {
             throw new ValidationException(result);
         }
+        comprobarDecanos(facultad.getId_decano());
         facultadService.findById(facultad.getId())
                 .orElseThrow(() -> new FacultadNoEncontradoException(facultad.getId()));
         Map<String, Object> response = new HashMap<>();
@@ -149,6 +129,17 @@ public class FacultadRestController {
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * Comprobar si el decano existe en el sistema.
+     */
+    private void comprobarDecanos(Long idDecano) {
+        Map<String, List<UsuarioDTO>> response = usuarioClient.idusuario();
+        List<UsuarioDTO> decanos = response.get("usuarios");
+        boolean existe = decanos.stream()
+                .anyMatch(d -> d.getId().equals(idDecano));
+        if (!existe) {
+            throw new RuntimeException(("El decano con id: " + idDecano + " no existe"));
+        }
+    }
 }
-
 
